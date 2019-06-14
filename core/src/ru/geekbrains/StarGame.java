03013f5ca3d69e91a415ab46a4a06153d505d8e5
vstartsev <vstartsev@amulex.ru>
2019-06-14 15:28:02 +0300

    use stage for background and add animation

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -2,33 +2,47 @@ package ru.geekbrains;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img,space;
 	TextureRegion face;
-	
+	Stage stage;
+	int imgpos = 0;
+	int imgposdir = 1;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		space = new Texture("space.jpg");
 		face = new TextureRegion(img,30,50,200,140);
+		stage = new Stage();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		imgpos+=imgposdir;
+		if (imgpos<0) imgposdir=-imgposdir;
+		if (imgpos>440) imgposdir=-imgposdir;
+		//Gdx.gl.glClearColor(0, 0, 0, 1);
+		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.getBatch().begin();
+		stage.getBatch().draw(space, 0, 0);
+		stage.getBatch().end();
+		stage.draw();
+
 		batch.begin();
+		//batch.setColor(0,0.5f,0,1);
+		//batch.draw(space, 0, 0);
 		batch.setColor(0,0.5f,0,1);
-		batch.draw(space, 0, 0);
-		batch.setColor(0.1f,0,0,1);
-		batch.draw(face, 100, 100);
+		batch.draw(face, imgpos, 100);
 		batch.end();
 	}
 	
